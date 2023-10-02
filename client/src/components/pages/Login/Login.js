4961ddd48b4abbe6f4f8177d6151063cbb7150d2
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2023-10-02 22:56:20 +0200

    Designed login and register I guess i will remake the rest once again in the future Now time for backend

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "../../style/loginRegister.module.scss";
 
 function Login() {
-  return <div className={styles["form-wrapper"]}>mmoże</div>;
+  return (
+    <div className={styles["form-wrapper"]}>
+      <div className={styles.form}>
+        <h1>Login</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        {/* Maybe forgot passwd here? */}
+        <button>Login</button>
+        <p>
+          No account? <NavLink to="/register">Sign up now!</NavLink>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/pages/Login/Register.js
+++ client/src/components/pages/Login/Register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "../../style/loginRegister.module.scss";
 
 function Register() {
@@ -6,10 +7,18 @@ function Register() {
     <div className={styles["form-wrapper"]}>
       <div className={styles.form}>
         <h1>Register</h1>
-        <input type="text" placeholder="Username" />
+        <div className={styles.names}>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <input type="password" placeholder="Confirm Password" />
+        {/* Maybe accept terms and conditions here? */}
         <button>Register</button>
+        <p>
+          Already have an account? <NavLink to="/login">Log in now!</NavLink>
+        </p>
       </div>
     </div>
   );
